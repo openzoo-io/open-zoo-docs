d17e0ae618f4e97a5b41792ba4a263afdda972a4
moohooooo <suwichalala@gmail.com>
2022-01-02 23:43:45 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/openzoo-io/open-zoo-docs',
+  //docsRepositoryBase: 'https://github.com/openzoo-io/open-zoo-docs',
+  titleSuffix: ' – OpenZoo Documentation',
   logo: (
     <>
       <img src="/openzoologo.png"/>
@@ -17,52 +17,51 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="OpenZoo is the NFT Marketplace of the Zoo Ecosystem, which features both artworks and game assets." />
       <meta name="og:description" content="OpenZoo is the NFT Marketplace of the Zoo Ecosystem, which features both artworks and game assets." />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://docs.openzoo.io/openzoodocs2.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="twitter:site:domain" content="docs.openzoo.io" />
+      <meta name="twitter:url" content="https://docs.openzoo.io/" />
       <meta name="og:title" content="OpenZoo: Documentation" />
       <meta name="og:image" content="https://docs.openzoo.io/openzoodocs2.png" />
       <meta name="apple-mobile-web-app-title" content="OpenZoo" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="/openzoo180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/openzoo192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/openzoo32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/favicon-96x96.png"
+        href="/openzoo96x96"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/openzoo16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/openzoo180x180.png" />
     </>
   ),
   search: true,
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
+  footerEditLink: false,
   footerText: <> {new Date().getFullYear()} © OpenZoo team.</>,
-  unstable_faviconGlyph: '👋',
 }
